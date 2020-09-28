9a1e2fb548503b3ed22453a41df7863a77494c4a
mmartofel <mmartofe@redhat.com>
2020-09-28 16:06:50 +0200

    create branch v02

--- target/classes/data.sql
+++ target/classes/data.sql
@@ -1,3 +1,3 @@
-insert into delikwent values (1,'Marek','Martofel','70072700000','Słaby','1.jpeg');
-insert into delikwent values (2,'Michał','Salwa','70072700000','Słaby','2.jpeg');
-insert into delikwent values (3,'Jarosław','Stakuń','70072700000','Słaby','3.jpeg');
+insert into delikwent values (1,'Marek','Martofel','XXXXXX00000','Szkoda gadać','1.jpeg');
+insert into delikwent values (2,'Michał','Salwa','YYYYYY00000','Zwyczajnie słaba','2.jpeg');
+insert into delikwent values (3,'Jarosław','Stakuń','ZZZZZZ00000','Bywała lepsza','3.jpeg');
