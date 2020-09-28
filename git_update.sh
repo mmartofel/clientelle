36cc9d43464e11941d97579b23d4faa7b0b73583
mmartofel <mmartofe@redhat.com>
2020-09-28 13:15:46 +0200

    create new branch v01

---
+++ git_update.sh
@@ -0,0 +1,4 @@
+git add .
+git commit -m "$1"
+git branch $2
+git push origin $2
---
+++ tekton-run-pipeline.sh
@@ -0,0 +1,1 @@
+tkn pipeline start build-and-deploy -r git-repo=clientelle-repo -r image=clientelle-image -p deployment-name=clientelle
