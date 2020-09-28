aa5b301f0c9c2f6a7f670d8845b96246fcccbb98
mmartofel <mmartofe@redhat.com>
2020-09-28 17:42:13 +0200

    1

--- tekton-run-pipeline.sh
+++ tekton-run-pipeline.sh
@@ -1,1 +1,18 @@
+
+if [ "$1" == "" ] && [ "$2" == "" ]; then
+
+ echo "Please provide two parameters:"
+
+else
+
+mvn clean package -DskipTests
+
+git add .
+git commit -m "$1"
+git branch $2
+git push origin $2
+
 tkn pipeline start build-and-deploy -r git-repo=clientelle-repo -r image=clientelle-image -p deployment-name=clientelle -l app=clientelle
+
+fi
+
