c833c9d050c985bf27d7903b3723f1d9cc9390bc
mmartofel <mmartofe@redhat.com>
2020-10-02 13:02:09 +0200

    Arrow / IBM demo

---
+++ mvn-build.sh
@@ -0,0 +1,1 @@
+mvn package oc:build oc:resource oc:deploy
--- oc-clean.sh
+++ oc-clean.sh
@@ -1,5 +1,6 @@
 APP=$1
 oc delete deployment $APP
+oc delete deploymentConfig $APP
 oc delete service $APP
 oc delete route $APP
 oc delete is/$APP
